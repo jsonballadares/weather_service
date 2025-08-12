05cbb9dc8b7ceecf487fdcfcaa4e9c2da643e4c1
Jason Balladares <jsonballadares@gmail.com>
2025-08-11 20:58:30 -0400

    adding main.go need to flesh out handlers.go for NWS calls

--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/jasonballadares/weather_service/api/routers"
+	"github.com/jasonballadares/weather_service/internal/env"
+)
 
 func main() {
-	e := e.initEnv()
+	e := env.InitEnv()
+
+	r := routers.InitRouter()
+
+	err := routers.Serve(r, e.Port)
+	if err != nil {
+		fmt.Println(fmt.Sprintf("Error starting server: %s", err))
+	}
 }
