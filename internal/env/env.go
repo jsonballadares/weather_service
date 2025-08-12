46db1a8d458c7910e62efa8bd572ce4d8033a73f
Jason Balladares <jsonballadares@gmail.com>
2025-08-11 20:42:08 -0400

    added env.go

--- internal/env/env.go
+++ internal/env/env.go
@@ -1,14 +1,24 @@
 package env
 
+// Base URL: https://api.weather.gov for NWS API
+
+import (
+	"fmt"
+)
+
 type Env struct {
-	Port string
+	Port       string
+	BaseUrlNWS string
 }
 
-func initEnv() *Env {
+func InitEnv() *Env {
+	fmt.Println("initEnv invoked")
 	e := &Env{
-		Port: "8080",
+		Port:       "8080",
+		BaseUrlNWS: "https://api.weather.gov",
 	}
 	return e
 }
 
 // would usually have some type of function here to load value's from OS or vault or kubernetes configMap using helm
+// would have port := os.Getenv("PORT") in there for example
